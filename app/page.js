1cc0acc995f5a9115f4ce8cceaa5ebb4f820ff34
roshan <roshanpanda666@gmail.com>
2025-04-25 20:13:46 +0530

    commiting a small change

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import Note from './components/note'
 const page = () => {
   return (
     <div>
+      <div className='flex gap-60 lg:gap-[70rem]'>
       <div className='text-1xl text-blue-300 font-bold font-mono lg:text-3xl'>Stable-Note</div>
+        <div className='text-blue-300 mt-2'>profile</div>
+      </div>
       <div>
         {/* components */}
         <Note></Note>
